50d1ce7b1be87a758e6d01e018426f36b410fc03
heoraengi <heo.raeng.i@gmail.com>
2024-04-06 13:58:44 +0900

    baekjoon 10819 ver0.2

--- Baekjoon/backtracking/차이를 최대로.py
+++ Baekjoon/backtracking/차이를 최대로.py
@@ -1,26 +1,43 @@
+from itertools import permutations
 n = int(input())
 arr = list(map(int, input().split()))
 max_num = 0
 
-
-def back(visited, lst):
-    global max_num
-
-    if len(lst) == n:
-        total = 0
-        for i in range(n-1):
-            total += abs(lst[i] - lst[i+1])
-        max_num = max(max_num, total)
-        return
-
-    for i in range(n):
-        if not visited[i]:
-            visited[i] = True
-            back(visited, lst + [arr[i]])
-            visited[i] = False
-
-
-visited = [False] * n
-back(visited, [])
+for a in permutations(arr):
+    total = 0
+    for i in range(n-1):
+        total += abs(a[i] - a[i+1])
+    max_num = max(max_num, total)
 
 print(max_num)
+
+# n = int(input())
+# arr = list(map(int, input().split()))
+# max_num = 0
+#
+#
+# def back(visited, lst):
+#     global max_num
+#
+#     if len(lst) == n:
+#         total = 0
+#         for i in range(n-1):
+#             total += abs(lst[i] - lst[i+1])
+#         max_num = max(max_num, total)
+#         return
+#
+#     for i in range(n):
+#         if not visited[i]:
+#             visited[i] = True
+#             back(visited, lst + [arr[i]])
+#             visited[i] = False
+#
+#
+# visited = [False] * n
+# back(visited, [])
+#
+# print(max_num)
+
+
+
+
