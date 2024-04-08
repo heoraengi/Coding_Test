b6f053211d3f58389c301271515b17916b061848
heoraengi <heo.raeng.i@gmail.com>
2024-04-08 16:42:10 +0900

    programmers 43162 - BFS.ver

--- Programmers/dfs_bfs/네트워크.py
+++ Programmers/dfs_bfs/네트워크.py
@@ -1,15 +1,40 @@
+# def solution(n, computers):
+#     def dfs(i, visited, computers):
+#         visited[i] = True
+#         for j in range(n):
+#             if computers[i][j] == 1 and not visited[j]:
+#                 dfs(j, visited, computers)
+#
+#     visited = [False] * n
+#     cnt = 0
+#     for i in range(n):
+#         if not visited[i]:
+#             dfs(i, visited, computers)
+#             cnt += 1
+#
+#     return cnt
+
+## BFS
+from collections import deque
+
+
 def solution(n, computers):
-    def dfs(i, visited, computers):
+    def bfs(i, visited, computers):
+        queue = deque([i])
         visited[i] = True
-        for j in range(n):
-            if computers[i][j] == 1 and not visited[j]:
-                dfs(j, visited, computers)
+
+        while queue:
+            q = queue.popleft()
+            for j in range(n):
+                if computers[q][j] == 1 and not visited[j]:
+                    queue.append(j)
+                    visited[j] = True
 
     visited = [False] * n
     cnt = 0
     for i in range(n):
         if not visited[i]:
-            dfs(i, visited, computers)
+            bfs(i, visited, computers)
             cnt += 1
 
     return cnt
