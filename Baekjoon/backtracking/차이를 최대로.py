9e4799b5644bfe2629ce1ed39a99790223f4e359
heoraengi <heo.raeng.i@gmail.com>
2024-05-13 17:01:54 +0900

    backjoon 23971

--- Baekjoon/backtracking/차이를 최대로.py
+++ Baekjoon/backtracking/차이를 최대로.py
@@ -11,32 +11,32 @@ for a in permutations(arr):
 
 print(max_num)
 
-# n = int(input())
-# arr = list(map(int, input().split()))
-# max_num = 0
-#
-#
-# def back(visited, lst):
-#     global max_num
-#
-#     if len(lst) == n:
-#         total = 0
-#         for i in range(n-1):
-#             total += abs(lst[i] - lst[i+1])
-#         max_num = max(max_num, total)
-#         return
-#
-#     for i in range(n):
-#         if not visited[i]:
-#             visited[i] = True
-#             back(visited, lst + [arr[i]])
-#             visited[i] = False
-#
-#
-# visited = [False] * n
-# back(visited, [])
-#
-# print(max_num)
+n = int(input())
+arr = list(map(int, input().split()))
+max_num = 0
+
+
+def back(visited, lst):
+    global max_num
+
+    if len(lst) == n:
+        total = 0
+        for i in range(n-1):
+            total += abs(lst[i] - lst[i+1])
+        max_num = max(max_num, total)
+        return
+
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            back(visited, lst + [arr[i]])
+            visited[i] = False
+
+
+visited = [False] * n
+back(visited, [])
+
+print(max_num)
 
 
 
--- Programmers/dfs_bfs/네트워크.py
+++ Programmers/dfs_bfs/네트워크.py
@@ -1,18 +1,18 @@
-# def solution(n, computers):
-#     def dfs(i, visited, computers):
-#         visited[i] = True
-#         for j in range(n):
-#             if computers[i][j] == 1 and not visited[j]:
-#                 dfs(j, visited, computers)
-#
-#     visited = [False] * n
-#     cnt = 0
-#     for i in range(n):
-#         if not visited[i]:
-#             dfs(i, visited, computers)
-#             cnt += 1
-#
-#     return cnt
+def solution(n, computers):
+    def dfs(i, visited, computers):
+        visited[i] = True
+        for j in range(n):
+            if computers[i][j] == 1 and not visited[j]:
+                dfs(j, visited, computers)
+
+    visited = [False] * n
+    cnt = 0
+    for i in range(n):
+        if not visited[i]:
+            dfs(i, visited, computers)
+            cnt += 1
+
+    return cnt
 
 ## BFS
 from collections import deque
